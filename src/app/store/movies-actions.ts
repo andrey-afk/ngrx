00d5bd6ef13f804a45e9947f3ee2f858f73db7eb
alexkolomoets <alexey.k@computools.com>
2021-12-20 15:05:25 +0200

    ngrx

--- src/app/store/movies-actions.ts
+++ src/app/store/movies-actions.ts
@@ -6,7 +6,3 @@ export const getMoviesSuccess = createAction('[Movies Component] Get all Movies 
 
 export const addToFavoriteMovie = createAction('[Movies Details Component] Add to favorite Movie', props<{favoriteMovies: TheMovies}>());
 export const deleteFavoriteMovie = createAction('[Main Info Component] Delete favorite Movie', props<{id: number | undefined}>());
-
-export const getMovieById = createAction('[Movies Details Component] Get Movie By Id Success', props<{id: number}>());
-
-export const goToNextMovie = createAction('[Movies Component] Show Next Movie', props<{index: number}>());
